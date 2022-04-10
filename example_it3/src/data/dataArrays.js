381d32736b306bc87069727dc7fad24d4bc9a5c2
jmcornelius1 <60238243+jmcornelius1@users.noreply.github.com>
2022-04-10 11:58:48 -0400

    Add files via upload

---
+++ example_it3/src/data/dataArrays.js
@@ -0,0 +1,119 @@
+export const categories = [
+  {
+    id: 3,
+    name: 'Sports',
+    photo_url:
+    'https://t4.ftcdn.net/jpg/00/57/47/87/240_F_57478764_QYgnIhRjrTwstCyemPELGxpup2LdvLHB.jpg'
+  },
+  {
+    id: 1,
+    name: 'The FAC',
+    photo_url: 'https://t4.ftcdn.net/jpg/03/27/40/43/240_F_327404352_LhgnB73OLd9SfpiIS36nipQ0l9HT7UrI.jpg'
+  },
+  {
+    id: 2,
+    name: 'About Us',
+    photo_url:
+      'https://t3.ftcdn.net/jpg/01/64/58/10/240_F_164581005_mzJwrB6nDqqnibEGd76Pq1Q1G3bcNNM0.jpg'
+  },
+  {
+    id: 4,
+    name: 'General Info.',
+    photo_url:
+    'https://t3.ftcdn.net/jpg/01/08/24/94/240_F_108249487_w8UD8P6YOYuh6Pr9NUw08juskfxpIBk4.jpg'
+  },
+  {
+    id: 0,
+    name: 'Events',
+    photo_url: 'https://t4.ftcdn.net/jpg/02/42/22/69/240_F_242226928_i4pNdSe7HAcc8DkQczpRA6csITpAo5ON.jpg'
+  },
+];
+
+export const recipes = [
+  {
+    recipeId: 122,
+    categoryId: 3,
+    title: 'Hours',
+    photo_url: 'https://t3.ftcdn.net/jpg/01/13/71/60/240_F_113716067_4whzGqglgcc0fmz8UsI8rnlnJW8zHbDC.jpg',
+    photosArray: [],
+  },
+  {
+    recipeId: 3,
+    categoryId: 4,
+    title: 'Member ID',
+    photo_url:
+      'https://t4.ftcdn.net/jpg/03/52/90/83/240_F_352908387_U5NSdLS8SQtx06N2pf2rUQJZdK9nV1Fp.jpg',
+    photosArray: [],
+    
+  },
+  {
+    recipeId: 2,
+    categoryId: 3,
+    title: 'Intramural Sports',
+    photo_url: 'https://t4.ftcdn.net/jpg/02/07/67/05/240_F_207670566_ZLpWArUIncY7BCZVbDdzktro02eyXQ9w.jpg',
+    photosArray: [],
+  },
+  {
+    recipeId: 3,
+    categoryId: 3,
+    title: 'Club Sports',
+    photo_url:
+      'https://cdn-icons-png.flaticon.com/128/1198/1198416.png',
+    photosArray: [],
+    
+  },
+  {
+    recipeId: 1,
+    categoryId: 1,
+    title: 'Aquatics',
+    photo_url: 'https://cdn-icons-png.flaticon.com/128/2932/2932366.png',
+  photosArray: [],
+  },
+  {
+    recipeId: 4,
+    categoryId: 1,
+    title: 'Climbing Wall',
+    photo_url: 'https://cdn-icons-png.flaticon.com/128/71/71468.png',
+    photosArray: [],
+     },
+  {
+    recipeId: 6,
+    categoryId: 1,
+    title: 'Group X',
+    photo_url:
+      'https://cdn-icons-png.flaticon.com/128/3314/3314631.png',
+    photosArray: [],
+  },
+  {
+    recipeId: 5,
+    categoryId: 1,
+    title: 'Personal Trainers',
+    photo_url:
+      'https://cdn-icons-png.flaticon.com/128/75/75929.png',
+    photosArray: [],
+  }, 
+  
+  {
+    recipeId: 0,
+    categoryId: 0,
+    title: 'Outdoor Adventures',
+    photo_url: 'https://cdn-icons-png.flaticon.com/128/1020/1020719.png',
+    photosArray: [],
+  },
+  {
+    recipeId: 7,
+    categoryId: 1,
+    title: 'Wellness',
+    photo_url: 'https://cdn-icons.flaticon.com/png/128/384/premium/384156.png?token=exp=1648166518~hmac=127ae80e187f2d88d10e19a7d5235404',
+    photosArray: [],
+  },
+  {
+    recipeId: 8,
+    categoryId: 2,
+    title: 'Student Employment',
+    photo_url: 'https://cdn-icons-png.flaticon.com/128/1239/1239608.png',
+    photosArray: [],
+    
+  }
+];
+
---
+++ example_it3/src/screens/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Home/HomeScreen';
+
+export default function Dashboard() {
+  return (
+    <HomeScreen/>
+  );
+}
